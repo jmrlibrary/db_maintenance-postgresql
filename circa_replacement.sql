f422fbd50d7db9c67f061e4090f834288f4c404b
Zach Weisser <zweisser@gmail.com>
2020-03-10 17:18:53 -0400

    circa_replacement works better

--- circa_replacement.sql
+++ circa_replacement.sql
@@ -1,23 +1,24 @@
 ﻿
 
+-- get all the scanned barcodes
 with first_loc as (
 
 select *  from sierra_view.item_view IV, sierra_view.subfield_view SV
- where barcode in ('31743303525222', '31743308404662', '31743308402161', '31743306094895')
+ where barcode in ('31743303525222', '31743308404662', '31743308402161', '31743306094895', '31743308754413')
  and IV.id = SV.record_id
  and SV.tag = 'a'
  and SV.field_type_code = 'c'
  and SV.record_type_code = 'i'
 ),
 
+-- get the tuple with the lowest call number
  min_call as (
-
 select * from first_loc order by first_loc.content asc limit 1
 
 ),
 
+-- get the tuple with the highest call number
 max_call as (
-
 select * from first_loc order by first_loc.content desc limit 1
 
 )
@@ -25,9 +26,12 @@ select * from first_loc order by first_loc.content desc limit 1
 -- select * from max_call
 -- union select * from min_call
 
-select *  from sierra_view.item_view IV, sierra_view.subfield_view SV
-where sv.content >= (select content from min_call limit 1)
-and sv.content <= (select content from max_call limit 1)
-and iv.location_code = 'rms'
+select location_code, content, barcode, inventory_gmt, item_status_code, due_gmt, *  
+from sierra_view.item_view IV join sierra_view.subfield_view SV on IV.id = sv.record_id
+left join sierra_view.checkout C on C.item_record_id = iv.id
 
-limit 10;
+where sv.content >= (select content from min_call)
+and sv.content <= (select content from max_call)
+and iv.location_code = 'rya' 
+
+order by content;
